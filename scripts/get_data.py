a36b29b7d2b4fc32b6d93b6dce64184da08caf28
insafich18 <74649594+insafich18@users.noreply.github.com>
2021-07-16 19:15:34 +0500

    Добавление UI
    В графическом интерфейсе есть поля для ввода данных. Сначала имя покупателя, потом количество банок, потом цена за банку. Снизу этих полей есть кнопка для занесения данных. Если нажать эту кнопку, появится вопрос с корректностью данных. При нажатии   'Yes'  начинается занос данных. При нажатии 'No' или крестика занос данных отменяется.  При успешном завершении заноса данных, появляется окно с уведомлением.

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,14 +1,45 @@
-class HoneyIncomeAccounting:
+import sys
+from PyQt5.QtWidgets import *
+
+
+class HoneyIncomeAccounting(QDialog):
     """Класс программы cобирающей данные и выовдящий результаты в output.txt."""
 
     def __init__(self):
+        super().__init__()
+        self.setWindowTitle("test")
+        self.resize(600, 600)
+        # Поля для ввода нужных данных.
+        self.led_customer_name = QLineEdit("Имя покупателя", self)
+        self.led_quantity_of_jars = QLineEdit("Количество банок меда:", self)
+        self.led_price_of_jar = QLineEdit("Цена за банку меда:", self)
+
+        #Расстановка полей.
+        self.led_customer_name.move(50, 40)
+        self.led_quantity_of_jars.move(50, 70)
+        self.led_price_of_jar.move(50, 100)
+
+        #Кнопка заноса данных.
+        self.btn_push_data = QPushButton("Занести данные", self)
+        self.btn_push_data.move(50, 130)
+
+        #Привязка к кнопке функции заноса данных.
+        self.btn_push_data.clicked.connect(self.get_customer_info)
+
         self.PATH_TO_OUTPUT = "D:\\GitHub projects\\honey_income_accounting\\txt_files\\output.txt"
 
     def get_customer_info(self):
         """Получение информации о покупке меда."""
-        customer_name = input("Имя покупателя:")
-        quantity_of_jars = int(input("Количество банок меда:"))
-        price_for_jar = int(input("Цена за банку меда:"))
+        #Проверка на валидабельность пользователем.
+        quest_correctness = QMessageBox.question(self, 
+                            "Проверка корректности", "Проверьте на корректность введенные даннные. Если все правильно нажмите 'Yes'.")
+        if quest_correctness == QMessageBox.No:
+            return 0
+
+        #Получение информации с полей.
+        customer_name = self.led_customer_name.text()
+        quantity_of_jars = int(self.led_quantity_of_jars.text())
+        price_for_jar = int(self.led_price_of_jar.text())
 
         with open(self.PATH_TO_OUTPUT, 'r', encoding="UTF-8") as file:
             # Вытаскивание данных.
@@ -20,7 +51,7 @@ class HoneyIncomeAccounting:
             new_quantity = old_quantity + quantity_of_jars
             new_price = old_price + (price_for_jar * quantity_of_jars)
             new_result = f"\nКоличество проданных банок: {new_quantity} Количество заработанных денег: {new_price} руб."
-            
+
             # Удаление устаревшой информации о цене и количестве купленных банок.
             del(file_data[-1])
 
@@ -31,8 +62,14 @@ class HoneyIncomeAccounting:
 
             for data in file_data:
                 file.write(data)
+        
+        #Показ пользователю, что все хорошо.
+        QMessageBox.information(self, "Успешно!", "Данные занесены!")
+        
 
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
     program = HoneyIncomeAccounting()
-    program.get_customer_info()
+    program.show()
+    sys.exit(app.exec_())
